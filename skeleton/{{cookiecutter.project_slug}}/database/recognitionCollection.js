4c08412add6b8db0d46e3124d9c9e8210a6ff59e
jknight-liatrio <joek@liatrio.com>
2021-06-25 14:44:39 -0500

    Using create-react-demo template skeleton instead of gratibot
    Just to make it work.

--- skeleton/{{cookiecutter.project_slug}}/database/recognitionCollection.js
+++
@@ -1,8 +0,0 @@
-const db = require('./db')
-const recognitionCollection = db.get('recognition')
-
-recognitionCollection.createIndex('recognizer')
-recognitionCollection.createIndex('recognizee')
-recognitionCollection.createIndex('timestamp')
-
-module.exports = recognitionCollection
--- skeleton/{{cookiecutter.project_slug}}/tf/backends/nonprod.tf
+++
@@ -1,4 +0,0 @@
-resource_group_name  = "gratibot-azure-data"
-storage_account_name = "gratibotazuredatatfnp"
-container_name       = "tfstate"
-key                  = "terraform.tfstate"
