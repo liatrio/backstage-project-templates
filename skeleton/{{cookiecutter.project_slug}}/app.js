fd04b49f30e975319e9aae37aa0c2a4c5959f1a3
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2021-07-07 12:42:06 -0700

    Clean up template

--- skeleton/{{cookiecutter.project_slug}}/app.js
+++ skeleton/{{cookiecutter.project_slug}}/app.js
@@ -1,21 +1,21 @@
 const { App } = require("@slack/bolt");
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.BOT_USER_OAUTH_ACCESS_TOKEN,
-  signingSecret: process.env.SIGNING_SECRET,
+    token: process.env.BOT_USER_OAUTH_ACCESS_TOKEN,
+    socketMode: true,
+    appToken: process.env.APP_TOKEN,
 });
 
 var normalizedPath = require("path").join(__dirname, "features");
 require("fs")
-  .readdirSync(normalizedPath)
-  .forEach(function (file) {
-    require("./features/" + file)(app);
-  });
+    .readdirSync(normalizedPath)
+    .forEach(function (file) {
+        require("./features/" + file)(app);
+    });
 
 (async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+    // Start your app
+    await app.start();
 
-  console.log("⚡️ Bolt app is running!");
+    console.log("⚡️ Bolt app is running!");
 })();
--- skeleton/{{cookiecutter.project_slug}}/tf/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {}
-}
-
-provider "azurerm" {
-  subscription_id = var.acr_subscription_id
-  features {}
-  skip_provider_registration = "true"
-}
